64fa2e8c9667f8a985d8abd3606a66d824c31e0a
Replit user <>
2023-05-09 06:13:57 +0000

    modified the hat class and defined the experiment method

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,25 +3,19 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-  def __init__(self, *args):
-    self.args = args
-    self.balls = {}
-    for arg in self.args:
-      color, number = arg.split('=')
-      self.balls[color] = int(number)
-    self.contents = []
-    for color, number in self.balls.items():
-      for i in range(number):
-        self.contents.append(color)
+  def __init__(self, **kwargs):
+    self.contents = [k for k, v in kwargs.items() for _ in range(v)]
 
-  def draw(self, num):
-    balls_bag = self.contents
-    draws = []
-    for i in range(num):
-      balls_left = len(balls_bag)
-      random_draw = random.randint(0, balls_left)
-      pulled_ball = balls_bag.pop(random_draw)
-      draws.append(pulled_ball)
-    return draws
+  def draw(self, n):
+    n = min(n, len(self.contents))
+    return [self.contents.pop(random.randrange(len(self.contents))) for _ in range(n)]
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  m = 0
+  for _ in range(num_experiments):
+    another_hat = copy.deepcopy(hat)
+    balls_drawn = another_hat.draw(num_balls_drawn)
+    balls_req = sum([1 for k, v in expected_balls.items() if balls_drawn.count(k) >= v])
+    m += 1 if balls_req == len(expected_balls) else 0
+
+  return m / num_experiments
